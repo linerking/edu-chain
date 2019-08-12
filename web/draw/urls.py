b709735861b81e359a5ec778d96213f180f8e783
906870065 <906870065@qq.com>
2019-08-12 18:11:54 -0400

    final version

--- web/draw/urls.py
+++ web/draw/urls.py
@@ -4,16 +4,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url('logging_School/', views.logging_School, name='logging_School'),
-  url('logging_Education/', views.logging_Education, name='logging_Education'),
+  url('login_school/', views.login_school, name='login_school'),
   url('choose_way/', views.choose_way, name='choose_way'),
-  url('Education_change/', views.Education_change, name='Education_change'),
-  url('Education_main/', views.Education_main, name='Education_main'),
-  url('employer_main/', views.employer_main, name='employer_main'),
   url('employer_search/', views.employer_search, name='employer_search'),
   url('employer_search_result/', views.employer_search_result, name='employer_search_result'),
   url('excel_upload/', views.excel_upload, name='excel_upload'),
-  url('logging_employer/', views.logging_employer, name='logging_employer'),
+  url('login_employer/', views.login_employer, name='login_employer'),
   url('manual_upload/', views.manual_upload, name='manual_upload'),
   url('school_main/', views.school_main, name='school_main'),
   url('student_change/', views.student_change, name='student_change'),
@@ -23,6 +19,9 @@ urlpatterns = [
   url('teacher_main/', views.teacher_main, name='teacher_main'),
   url('teacher_search/', views.teacher_search, name='teacher_search'),
   url('teacher_upload/', views.teacher_upload, name='teacher_upload'),
+  url('permission/', views.add_permission, name='add_permission'),
+  url('school_login',views.school_login,name="school_login"),
+  url('employer_login',views.employer_login,name="employer_login"),
   url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
 
